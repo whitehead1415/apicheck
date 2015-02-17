3ee2c57987875fac7f2b5a469788d713b71d3a8b
Michael Whitehead <michael.whitehead@mediaforge.com>
2015-02-17 16:00:25 -0700

    Still feeling it out

--- Main.hs
+++ Main.hs
@@ -3,13 +3,15 @@
 
 import Prelude hiding (read)
 import Test.QuickCheck
+import Test.QuickCheck.Monadic  (assert, monadicIO, pick, pre, run)
 import Network.Wreq
 import Control.Monad
 import Control.Applicative
 import Control.Lens hiding (elements)
 import Data.Aeson
 import GHC.Generics
-import Data.Either
+
+apiEndPoint = "http://localhost:3000/api/users"
 
 --Models
 
@@ -38,6 +40,7 @@ instance (FromJSON a) => FromJSON (Resp a) where
     Resp <$> parseJSON obj
     <|> JsonError <$> v .: "error"
     <|> pure DecodingError 
+  parseJSON _ = mzero
               
 data User = User { userName :: Name
                  , userEmail :: Email
@@ -52,11 +55,6 @@ instance ToJSON JsonMsg
 instance FromJSON User
 instance ToJSON User
 
-data Valid a = Valid a deriving (Show)
-data Invalid a = Invalid a deriving (Show)
-
-newtype Tester a = Tester a
-
 -- Generators
 
 validName = fmap Name $ elements ["name1", "name2", "name3"]
@@ -65,8 +63,8 @@ invalidName = fmap Name $ elements ["invalidName1", "invalidName2", "invalidName
 validEmail = fmap Email $ elements ["email1", "email2", "email3"]
 invalidEmail = fmap Email $ elements ["invalidEmail1", "invalidEmail2", "invalidEmail3"]
 
-validPass = fmap Password $ elements ["invalidPass1", "invalidPass2", "invalidPass3"]
-invalidPass = fmap Password $ elements ["pass1", "pass2", "pass3"]
+validPass = fmap Password $ elements ["pass1", "pass2", "pass3"]
+invalidPass = fmap Password $ elements ["invalidPass1", "invalidPass2", "invalidPass3"]
 
 -- Generating Instances
 
@@ -90,37 +88,82 @@ instance Variant User where
   valid = liftM3 User valid valid valid
   invalid = liftM3 User invalid invalid invalid
 
-instance (Variant a, CRUD a) => Arbitrary (Tester a) where
-  arbitrary = liftM Tester $ frequency [(1, valid)]
+data CrudOperation a = Create (a -> IO (Response (Resp a)))
+                     | Read (String -> IO (Response (Resp a)))
+                     | List (IO (Response [Resp a]))
+                     | Update (a -> String -> IO (Response (Resp a)))
+                     | Delete (a -> String -> IO (Response (Resp JsonMsg)))
 
---instance (Variant a) => Arbitrary (Valid a) where
-  --arbitrary = liftM Valid valid
+instance Show (CrudOperation a) where
+  show (Create _) = "a -> IO (Response (Resp a))"
+  show (Read _) = "String -> IO (Response (Resp a))"
+  show (List _) = "IO (Response [Resp a])"
+  show (Update _) = "a -> String -> IO (Response (Resp a))"
+  show (Delete _) = "a -> String -> IO (Response (Resp JsonMsg))"
 
---instance (Variant a) => Arbitrary (Invalid a) where
-  --arbitrary = liftM Invalid invalid
+instance (Resource a) => Arbitrary (CrudOperation a) where
+  arbitrary = frequency [(1, elements [Create create])
+                        ,(1, elements [Read read])
+                        ,(1, elements [List list])
+                        ,(1, elements [Update update])
+                        ,(1, elements [Delete del])
+                        ]
 
---instance (Variant a) => Arbitrary a where
-  --arbitrary = frequency [(8, valid), (2, invalid)]
+data Varied a = Valid a
+              | Invalid a deriving Show
 
---instance Arbitrary User where
-  --arbitrary = frequency [(8, valid . create), (2, invalid)]
+data ResourceTester a = ResourceTester { testModel :: Varied a
+                                       , operation :: CrudOperation a
+                                       } deriving Show
 
-class CRUD a where
+instance (Variant a, Resource a) => Arbitrary (ResourceTester a) where
+  arbitrary = do
+    model <- liftM Valid $ frequency [(1, valid), (1, invalid)]
+    op <- arbitrary
+    return $ ResourceTester model op
+
+class Resource a where
   create :: a -> IO (Response (Resp a))
-  --read :: Gen -> String -> IO (Response (Resp a))
-  --list :: Gen -> IO (Response (Resp [a]))
-  --update :: Gen a -> String -> IO (Response (Resp a))
-  --del :: Gen -> a -> String -> IO (Response (Resp JsonMsg))
+  read :: String -> IO (Response (Resp a))
+  list :: IO (Response [Resp a])
+  update :: a -> String -> IO (Response (Resp a))
+  del :: a -> String -> IO (Response (Resp JsonMsg))
 
-apiEndPoint = "http://localhost:3000/api/users"
-
-instance CRUD User where
+instance Resource User where
   create user = asJSON =<< post apiEndPoint (toJSON user)
-  --read id = asJSON =<< get (apiEndPoint ++ id)
-  --list = asJSON =<< get apiEndPoint
-  --update user id = asJSON =<< put (apiEndPoint ++ id) (toJSON user)
-  --del user id = asJSON =<< delete (apiEndPoint ++ id)
+  read id = asJSON =<< get (apiEndPoint ++ "/" ++ id)
+  list = asJSON =<< get apiEndPoint
+  update user id = asJSON =<< put (apiEndPoint ++ "/" ++ id) (toJSON user)
+  del user id = asJSON =<< delete (apiEndPoint ++ "/" ++ id)
 
--- Properties
+getInner :: Varied a -> a
+getInner (Valid a) = a 
+getInner (Invalid a) = a
 
+prop_tester_user :: ResourceTester User -> Property
+prop_tester_user (ResourceTester model (Create op)) =
+  monadicIO $ do
+    res <- run $ op $ getInner model
+    assert True
+prop_tester_user (ResourceTester model (Read op)) =
+  monadicIO $ do
+    res <- run $ op "foo"
+    assert True
+prop_tester_user (ResourceTester model (List op)) =
+  monadicIO $ do
+    res <- run op 
+    assert True
+prop_tester_user (ResourceTester model (Update op)) =
+  monadicIO $ do
+    res <- run $ op (getInner model) "foo"
+    assert True
+prop_tester_user (ResourceTester model (Delete op)) =
+  monadicIO $ do
+    res <- run $ op (getInner model) "foo"
+    assert True
+
+  -- Properties
+main :: IO()
+--main = quickCheck prop_tester_user
+--main = sample (arbitrary :: Gen (ResourceTester User))
 main = putStrLn "FOO"
